882f2f0422bccf32cb82286e03f114de26c1f653
Arthur Diniz <arthur_almerio@hotmail.com>
2018-08-09 17:00:32 -0300

    atualizando

--- config/app.php
+++ config/app.php
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'dAOYgwoyeexXCsQbJdLjyLkoB0p9WF1kIqHufnFpBXM'),
+    'key' => 'dAOYgwoyeexXCsQbJdLjyLkoB0p9WF1kIqHufnFpBXM',
 
     'cipher' => 'AES-256-CBC',
 
